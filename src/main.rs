03b0d164f53673d34342c8d4ef6e66d4eacd89b9
Jordi Pujol Ricarte <jpricarte@inf.ufrgs.br>
2021-09-18 10:36:32 -0300

    It's working

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,46 @@
 use rand::Rng;
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::{Arc, Mutex};
 use std::thread;
-use std::time::Duration;
+use std::time::Instant;
 
-mod mergesort;
-
+mod multi_thread;
+mod mono_thread;
 
 fn main() {
     // Number used to generate vector (size = GEN_THREADS * NUM_PER_THREADS)
-    const GEN_THREADS: usize = 3;
-    const NUM_PER_THREADS: usize = 10;
-    let numbers = Arc::new(Mutex::new(Vec::<isize>::new()));
-
+    const GEN_THREADS: usize = 1 << 4;
+    const NUM_PER_THREADS: usize = 1 << 10;
+    const MIN_SIZE: usize = 1 << 11;
+    let mono_numbers = Arc::new(Mutex::new(Vec::<isize>::new()));
+    let multi_numbers = Arc::new(Mutex::new(Vec::<isize>::new()));
+    
     //Generate vector using threads
-    for _ in 0..GEN_THREADS {
-        let numbers = Arc::clone(&numbers);
-        thread::spawn(move || { // Populating vector
+    let mut threads = Vec::new();
+    for _ in 1..=GEN_THREADS {
+        let mono_numbers = Arc::clone(&mono_numbers);
+        let multi_numbers = Arc::clone(&multi_numbers);
+        threads.push(thread::spawn(move || { // Populating vector
             let mut rng = rand::thread_rng();
-            let mut v = numbers.lock().unwrap();
+            let mut mono = mono_numbers.lock().unwrap();
+            let mut multi = multi_numbers.lock().unwrap();
             for _ in 1..=NUM_PER_THREADS {
-                v.push(rng.gen::<isize>());
+                let number = rng.gen::<isize>();
+                mono.push(number);
+                multi.push(number);
             }
-        });
+        }));
     }
+    for t in threads {
+        t.join().unwrap();
+    }
+    let start_mono = Instant::now();
+    mono_thread::mergesort(Arc::clone(&mono_numbers), 0, GEN_THREADS*NUM_PER_THREADS);
+    let end_mono = start_mono.elapsed();
 
-    {println!("{:?}", numbers.lock().unwrap())};
-    mergesort::mergesort(Arc::clone(&numbers), 0, (GEN_THREADS*NUM_PER_THREADS) - 1);
-    println!("{:?}", numbers.lock().unwrap());
+    let start_multi = Instant::now();
+    multi_thread::mergesort(Arc::clone(&multi_numbers), 0, GEN_THREADS*NUM_PER_THREADS, MIN_SIZE);
+    let end_multi = start_multi.elapsed();
+
+    println!("Single Thread elapsed time: {}ms", end_mono.as_millis());
+    println!("Multi Thread elapsed time: {}ms", end_multi.as_millis());
 }
